4abbbd999a535cd19d809b7b9cb7f58597b60af3
Peter Arnold <peter@deepsixdesign.com>
2019-12-11 18:38:55 +0000

    asd

--- server.js
+++ server.js
@@ -1,96 +1,89 @@
-const express = require('express')
-const session = require('express-session')
-const cors = require('cors');
-const bodyParser = require('body-parser')
-const FileStore = require('session-file-store')(session)
-const next = require('next')
-const admin = require('firebase-admin')
+const express = require("express");
+const session = require("express-session");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const FileStore = require("session-file-store")(session);
+const next = require("next");
+const admin = require("firebase-admin");
 
-const { join } = require('path')
-const { parse } = require('url')
+const { join } = require("path");
+const { parse } = require("url");
 
-const port = parseInt(process.env.PORT, 10) || 3000
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const port = parseInt(process.env.PORT, 10) || 3000;
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-    const server = express()
-    const { serverRuntimeConfig, publicRuntimeConfig } = require('next/config').default();
-    const { type, project_id, private_key_id, private_key, client_email, client_id, auth_uri, token_uri, auth_provider_x509_cert_url, client_x509_cert_url } = serverRuntimeConfig
-    const firebase = admin.initializeApp(
-        {
-            credential: admin.credential.cert(
-                {
-                    "type": type,
-                    "project_id": project_id,
-                    "private_key_id": private_key_id,
-                    "private_key": private_key,
-                    "client_email": client_email,
-                    "client_id": client_id,
-                    "auth_uri": auth_uri,
-                    "token_uri": token_uri,
-                    "auth_provider_x509_cert_url": auth_provider_x509_cert_url,
-                    "client_x509_cert_url": client_x509_cert_url
-                }
-            ),
-        },
-        'server'
-    )
+  const server = express();
+  //   const {
+  //     serverRuntimeConfig,
+  //     publicRuntimeConfig
+  //   } = require("next/config").default();
+  //   const {
+  //     type,
+  //     project_id
+  //   } = serverRuntimeConfig;
+  const firebase = admin.initializeApp(
+    {
+      credential: admin.credential.cert(require("./credentials/server"))
+    },
+    "server"
+  );
 
-    server.use(cors());
-    server.use(bodyParser.json())
+  server.use(cors());
+  server.use(bodyParser.json());
 
-    server.use(
-        session({
-            secret: 'geheimnis',
-            saveUninitialized: true,
-            store: new FileStore({ secret: 'geheimnis' }),
-            resave: false,
-            rolling: true,
-            httpOnly: true,
-            cookie: { maxAge: 604800000 }, // week
-        })
-    )
-
-    server.use((req, res, next) => {
-        req.firebaseServer = firebase
-        next()
+  server.use(
+    session({
+      secret: "geheimnis",
+      saveUninitialized: true,
+      store: new FileStore({ secret: "geheimnis" }),
+      resave: false,
+      rolling: true,
+      httpOnly: true,
+      cookie: { maxAge: 604800000 } // week
     })
+  );
 
-    server.post('/service-worker.js', (req, res) => {
-        const parsedUrl = parse(req.url, true)
-        const { pathname } = parsedUrl
-        const filePath = join(__dirname, '.next', pathname)
-        app.serveStatic(req, res, filePath)
-    })
+  server.use((req, res, next) => {
+    req.firebaseServer = firebase;
+    next();
+  });
 
-    server.post('/api/login', (req, res) => {
-        if (!req.body) return res.sendStatus(400)
+  server.post("/service-worker.js", (req, res) => {
+    const parsedUrl = parse(req.url, true);
+    const { pathname } = parsedUrl;
+    const filePath = join(__dirname, ".next", pathname);
+    app.serveStatic(req, res, filePath);
+  });
 
-        const token = req.body.token
-        firebase
-            .auth()
-            .verifyIdToken(token)
-            .then(decodedToken => {
-                req.session.decodedToken = decodedToken
-                return decodedToken
-            })
-            .then(decodedToken => res.json({ status: true, decodedToken }))
-            .catch(error => res.json({ error }))
-    })
+  server.post("/api/login", (req, res) => {
+    if (!req.body) return res.sendStatus(400);
 
-    server.post('/api/logout', (req, res) => {
-        req.session.decodedToken = null
-        res.json({ status: true })
-    })
+    const token = req.body.token;
+    firebase
+      .auth()
+      .verifyIdToken(token)
+      .then(decodedToken => {
+        req.session.decodedToken = decodedToken;
+        return decodedToken;
+      })
+      .then(decodedToken => res.json({ status: true, decodedToken }))
+      .catch(error => res.json({ error }));
+  });
 
-    server.all('*', (req, res) => {
-        return handle(req, res)
-    })
+  server.post("/api/logout", (req, res) => {
+    req.session.decodedToken = null;
+    res.json({ status: true });
+  });
 
-    server.listen(port, err => {
-        if (err) throw err
-        console.log(`> Ready on http://localhost:${port}`)
-    })
-})
+  server.all("*", (req, res) => {
+    return handle(req, res);
+  });
+
+  server.listen(port, err => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,116 +1,118 @@
 //React
-import React, { useContext, Component } from 'react'
+import React, { useContext, Component } from "react";
 
 // Firebase
-import firebase from 'firebase/app'
+import firebase from "firebase/app";
 import "firebase/storage";
-import 'firebase/auth'
-import 'firebase/firestore'
-import 'isomorphic-unfetch'
+import "firebase/auth";
+import "firebase/firestore";
+import "isomorphic-unfetch";
 
 // Nextjs
-import App from 'next/app';
-import Head from 'next/head';
-import Router from 'next/router';
+import App from "next/app";
+import Head from "next/head";
+import Router from "next/router";
 
 // Material-ui
-import { ThemeProvider } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import theme from '../config/theme';
+import { ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import theme from "../config/theme";
 
 // Auth & User context
-import UserContext from '../lib/UserContext'
+import UserContext from "../lib/UserContext";
+
+// Firebase Creds
+import clientCredentials from "../../credentials/client";
 
 // Components
-import NavHeader from '../components/NavHeader'
+import NavHeader from "../components/NavHeader";
 
 // Nextjs env config
-import getConfig from 'next/config'
-const { publicRuntimeConfig } = getConfig()
-const { apiKey, authDomain, databaseURL, projectId, storageBucket, messagingSenderId, appId, measurementId } = publicRuntimeConfig
+// import getConfig from "next/config";
+// const { publicRuntimeConfig } = getConfig();
+// const {
+//   apiKey,
+//   authDomain
+// } = publicRuntimeConfig;
 
-let storage = null
-let firestore = null
+let storage = null;
+let firestore = null;
 
 // Main export
 export default class MyApp extends App {
-
   static async getInitialProps({ req, query }) {
-    const user = req && req.session ? req.session.decodedToken : null
+    const user = req && req.session ? req.session.decodedToken : null;
     // don't fetch anything from firebase if the user is not found
     // const snap = user && await req.firebaseServer.database().ref('messages').once('value')
     // const messages = snap && snap.val()
-    const messages = null
+    const messages = null;
 
-    return { user, messages }
+    return { user, messages };
   }
 
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      user: this.props.user,
-    }
+      user: this.props.user
+    };
   }
 
   componentDidMount() {
     if (!firebase.apps.length) {
-      firebase.initializeApp({
-        apiKey: apiKey,
-        authDomain: authDomain,
-        databaseURL: databaseURL,
-        projectId: projectId,
-        storageBucket: storageBucket,
-        messagingSenderId: messagingSenderId,
-        appId: appId,
-        measurementId: measurementId
-      })
+      firebase.initializeApp(clientCredentials);
       storage = firebase.storage();
       firestore = firebase.firestore();
     }
     firebase.auth().onAuthStateChanged(user => {
       if (user) {
-        this.setState({ user: user })
-        return user
-          .getIdToken()
-          .then(token => {
+        this.setState({ user: user });
+        return user.getIdToken().then(token => {
+          // eslint-disable-next-line no-undef
+          return fetch("/api/login", {
+            method: "POST",
             // eslint-disable-next-line no-undef
-            return fetch('/api/login', {
-              method: 'POST',
-              // eslint-disable-next-line no-undef
-              headers: new Headers({ 'Content-Type': 'application/json' }),
-              credentials: 'same-origin',
-              body: JSON.stringify({ token }),
-            })
-          })
+            headers: new Headers({ "Content-Type": "application/json" }),
+            credentials: "same-origin",
+            body: JSON.stringify({ token })
+          });
+        });
       } else {
-        this.setState({ user: null })
+        this.setState({ user: null });
         // eslint-disable-next-line no-undef
-        fetch('/api/logout', {
-          method: 'POST',
-          credentials: 'same-origin',
-        })
+        fetch("/api/logout", {
+          method: "POST",
+          credentials: "same-origin"
+        });
       }
-    })
+    });
     // Remove the server-side injected CSS.
-    const jssStyles = document.querySelector('#jss-server-side');
+    const jssStyles = document.querySelector("#jss-server-side");
     if (jssStyles) {
       jssStyles.parentElement.removeChild(jssStyles);
     }
   }
 
   handleLogin = () => {
-    firebase.auth().signInWithPopup(new firebase.auth.GoogleAuthProvider())
+    firebase.auth().signInWithPopup(new firebase.auth.GoogleAuthProvider());
   };
 
   handleLogout = () => {
-    firebase.auth().signOut()
+    firebase.auth().signOut();
   };
 
   render() {
     const { Component, pageProps } = this.props;
 
     return (
-      <UserContext.Provider value={{ firestore: firestore, storage: storage, user: this.state.user, handleLogin: this.handleLogin, handleLogout: this.handleLogout }}>
+      <UserContext.Provider
+        value={{
+          firestore: firestore,
+          storage: storage,
+          user: this.state.user,
+          handleLogin: this.handleLogin,
+          handleLogout: this.handleLogout
+        }}
+      >
         <Head>
           <title>Plexus Prediction Engine </title>
         </Head>
